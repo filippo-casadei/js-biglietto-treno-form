d50464c7fb99f8a484269730a2e7f61d27cf1d75
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-03 15:41:57 +0200

    Copiato e commentato sul file script.js il vecchio js relativo allo stesso esercizio, per avere una linea guida su come funzionava precedentemente.

---
+++ script.js
@@ -0,0 +1,31 @@
+
+// // prompt per chiedere i km da percorrere:
+// let quantiKm = parseInt ( prompt ("Quanti km devi percorrere?" ));
+
+// // prompt per chiedere l'età:
+// let quantiAnniHai = parseInt ( prompt ("Quanti anni hai?" ));
+
+
+// // costo di 1km che NON cambia:
+// const prezzoKm = 0.21;
+
+// // variabile per calcolare il prezzo del biglietto base:
+// let prezzoBase = prezzoKm * quantiKm;
+
+// // variabile per calcolare gli sconti:
+// if (quantiAnniHai < 18) {
+//     prezzoBase = prezzoBase - (prezzoBase * 0.20);
+// }
+// else if (quantiAnniHai >= 65) {
+//     prezzoBase = prezzoBase - (prezzoBase * 0.40);
+// }
+// else {
+//     prezzoBase = prezzoKm * quantiKm;
+// }
+
+
+// // per stampare il risultato: 
+// console.log(`Il prezzo del biglietto è: ${prezzoBase.toFixed(2)}`)
+
+// // per vedere il risultato a schermo:
+// alert(`Il prezzo del biglietto è: ${prezzoBase.toFixed(2)}`)
